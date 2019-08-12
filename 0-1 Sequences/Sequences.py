a7b447b2c7de11dbc7bd96842c2ea2f38b1c3f35
Robert Peter Mulligan <rpmulligan@linux1.cs.uchicago.edu>
2019-08-12 14:31:52 -0500

    Possible working solution that needs optimization. Tries all possiblities. Fails third test on Kattis due to time limit. It's possible that the optimization needs to have a pattern recognition to it.

--- 0-1 Sequences/Sequences.py
+++ 0-1 Sequences/Sequences.py
@@ -5,48 +5,60 @@ import sys
 x = str(sys.stdin.readline())
 y = list(x)
 y.remove('\n')
+#y = ['?', '?', '?', '?', '1', '0', '1', '1', '?', '?']
 
 #----- program logic ------
 
+# global  variables
 ans = 0
-k_count = 0
 
-for elm in y:
-        if elm is '?':
-                k_count = k_count + 1
+def locations(seq):
+        k_locations = []
+        one_locations = []
+        zero_locations = []
+
+        # put the locations in respective arrays
+        for x in xrange(len(seq)): 
+	        if seq[x] is '?':
+		        k_locations.append(x)
+	        elif seq[x] is '1':
+		        one_locations.append(x)
+	        elif seq[x] is '0':
+		        zero_locations.append(x)
+	        else: 
+		        raise Exception('Not a 0, 1, or ?')
+        return [one_locations, zero_locations, k_locations, len(seq), seq]
 
 
+def seq_generator(locs):
 
-
-
-
-
-
-
-
-'''
-                def solver_r(input, ans):
-        for x in xrange(len(input)):
-               
-                print (input)
+        swap_count = 0
         
-                if input[x] is  '?':
-                        one_input = input[:]
-                        one_input[x] = '1'
-                        ans = ans + solver_r(one_input, 0)
-                        zero_input = input[:]
-                        zero_input[x] = '0'
-                        ans = ans + solver_r(zero_input, 0)
+        for x in xrange(2**len(locs[2])):
+                holder = locs[4]
+                k_holder = list(format(x,'#0'+str(len(locs[2])+2)+'b')[2:])
 
-                elif input[x] is '1' and x+1 is not len(input) and input[x+1] is '0':
-                        ans = ans + 1
-                        input[x] = '0'
-                        input[x+1] = '1'
-                        #  ans = ans + solver_r(swap_input, 0)
-        return ans
+                for k in xrange(len(locs[2])):
+                        holder[locs[2][k]] = k_holder.pop()
+                        
+                swap_count = swap_counter(holder) + swap_count
+                #print locations(holder)
+                        
+        return swap_count
 
+def swap_counter(seq_filled):
+        counter = 0
+        calculations = locations(seq_filled)
+        for x in reversed(xrange(len(calculations[1]))):
+                        if len(calculations[0]) is 0:
+                                return counter
 
-        
-print(solver_r(y, 0)  % (10**9 +7))
+                        if calculations[1][x] > calculations[0][0]:
+                                counter = calculations[1][x] - calculations[0][0]
+                                calculations[0] = calculations[0][1:]
+                       # print calculations
+        return counter
 
-'''
+#print locations(y)
+print (seq_generator(locations(y)) % (10**9 +7))
+
